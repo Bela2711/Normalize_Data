e2e00d41eed70b3ab4dfd9d5376c9a5ff41f307e
Bela Mhasavade <bela2711@gmail.com>
2015-08-02 13:11:02 -0700

    parsing csv file

--- Deal_A_Day/app/models/upload_file.rb
+++ Deal_A_Day/app/models/upload_file.rb
@@ -1,9 +1,11 @@
+require "csv"
 class UploadFile < ActiveRecord::Base
   def self.save(upload)
     #puts File.read(upload)
     #data =  String.read(upload['datafile'])
     #data = File.read(upload['datafile'])
-    puts upload.read
+    csv = CSV.new(upload.read,:headers => true,:header_converters => :symbol)
+    csv.to_a.map{|row| puts row.to_hash}
     
   end
 end
