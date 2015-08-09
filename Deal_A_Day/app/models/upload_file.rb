0819b0f99c39f3d73e4f0c9f6dd9fa5ea6732b45
Bela Mhasavade <bela2711@gmail.com>
2015-08-09 15:05:09 -0700

    completed the required functionality functionality without CSS

--- Deal_A_Day/app/models/upload_file.rb
+++ Deal_A_Day/app/models/upload_file.rb
@@ -1,6 +1,11 @@
 require "csv"
 class UploadFile
-  def self.save(file_data,user_id)
+attr_accessor :revenue
+  def initialize(user_id)
+    @user_id = user_id
+    @revenue = 0
+  end
+  def save(file_data)
     csv = CSV.new(file_data.read,:headers => true,:header_converters => :symbol)
     file_data.rewind
     if csv.to_a[1].to_s.split("\t")[1].nil?
@@ -12,16 +17,22 @@ class UploadFile
     end
     parsed_file.to_a.map{|row| 
        hashed_data = row.to_hash
-puts hashed_data
+       @revenue = @revenue + save_data(hashed_data)
+    }
+puts @revenue
+  end
+
+private
+def save_data(hashed_data)
        purchaser = Purchaser.create(name: hashed_data[:purchaser_name])
        merchant = Merchant.create(name: hashed_data[:merchant_name],address: hashed_data[:merchant_address])
        item = Item.create(description: hashed_data[:item_description])
-puts "here yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz"
 p_id = Purchaser.find_by_name(hashed_data[:purchaser_name]).id
 m_id = Merchant.find_by_name(hashed_data[:merchant_name]).id
 i_id = Item.find_by_description(hashed_data[:item_description]).id
-order = Order.create(purchaser_id: p_id, item_id: i_id, count: hashed_data[:purchase_count].to_i, user_id: user_id.to_i)
+order = Order.create(purchaser_id: p_id, item_id: i_id, count: hashed_data[:purchase_count].to_i, user_id: @user_id.to_i)
 item_merchant = ItemsMerchant.create(item_id: i_id,merchant_id: m_id, price: hashed_data[:item_price])
-    }
-  end
+
+hashed_data[:item_price].to_f * hashed_data[:purchase_count].to_i
+end 
 end
